2f61cddbb161dadc55e45c7008c5f0810f1fdb41
Sean <ysfelf@qq.com>
2018-01-31 22:43:29 +0800

    first test

---
+++ src/Talker.cpp
@@ -0,0 +1,35 @@
+//
+// Created by yubun on 18-1-23.
+//
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle node;
+    ros::Publisher chatter_pub = node.advertise<std_msgs::String>("chatter", 1000);
+    ros::Rate loop_rate(10);
+
+
+    int count = 0;
+    while (ros::ok())
+    {
+        std_msgs::String msg;
+
+        std::stringstream ss;
+        ss<<"hello world"<<"+"<< count<<"dd";
+        msg.data = ss.str();
+        ROS_INFO("%s", msg.data.c_str());
+
+        chatter_pub.publish(msg);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+        count++;
+    }
+    return 0;
+}
